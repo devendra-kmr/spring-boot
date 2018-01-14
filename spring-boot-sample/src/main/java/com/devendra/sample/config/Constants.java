fb41375bc26c9f80f28d0ac2377a512e1fe6ee07
Devendra <devendrakumar.gzp@outlook.com>
2018-01-14 23:19:29 +0530

    added class

---
+++ spring-boot-sample/src/main/java/com/devendra/sample/config/Constants.java
@@ -0,0 +1,20 @@
+package com.devendra.sample.config;
+
+/**
+ * Created by devendra on 7/26/17.
+ *
+ * Application constants.
+ */
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    public static final String SPRING_PROFILE_CLOUD = "cloud";
+
+
+}
